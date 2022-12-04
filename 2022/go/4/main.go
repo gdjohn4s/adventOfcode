d40dbd413fafcba07b4ffdf8bf7e690d6ba9e0d7
gdjohn4s <john4s@protonmail.ch>
2022-12-04 22:55:38 +0100

    added day 4 puzzle 1

---
+++ 2022/go/4/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+const filename string = "input.prod"
+
+func main() {
+	puzzle1()
+}
+
+/* Day 4 Puzzle 1 */
+func puzzle1() {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+
+	var stronzi int = 0
+	for sc.Scan() {
+		var first, lastFirst, second, lastSecond int
+		fmt.Sscanf(sc.Text(), "%d-%d,%d-%d", &first, &lastFirst, &second, &lastSecond)
+		if second >= first && lastSecond <= lastFirst || first >= second && lastFirst <= lastSecond {
+			stronzi += 1
+		}
+
+		fmt.Println(stronzi)
+	}
+}
+
