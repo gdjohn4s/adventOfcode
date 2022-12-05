e87fb0388cf5d34bee898bc11054cd23c435a779
gdjohn4s <john4s@protonmail.ch>
2022-12-05 21:10:32 +0100

    day 4 puzzle 2

--- 2022/go/4/main.go
+++ 2022/go/4/main.go
@@ -34,3 +34,24 @@ func puzzle1() {
 	}
 }
 
+/* Day 4 Puzzle 2 */
+func puzzle2() {
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	sc := bufio.NewScanner(file)
+
+	var stronzi int = 0
+	for sc.Scan() {
+		var first, lastFirst, second, lastSecond int
+		fmt.Sscanf(sc.Text(), "%d-%d,%d-%d", &first, &lastFirst, &second, &lastSecond)
+		if second <= lastFirst && lastSecond >= first || first <= second && lastFirst >= lastSecond {
+			stronzi += 1
+		}
+	}
+	fmt.Println(stronzi)
+}
+
